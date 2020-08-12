013d08d78424e2606b652be36547d48e433e05e9
Joao Hortale <jhortale@gmail.com>
2020-08-11 21:42:42 -0300

    feature/testing enviroment setup

---
+++ src/__tests__/Pages/SignIn.spec.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+
+import SignIn from '../../pages/SignIn';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(),
+  };
+});
+
+describe('SignIn page', () => {
+  it('should contains email/ password input', () => {
+    const { getByPlaceholderText } = render(<SignIn />);
+
+    expect(getByPlaceholderText('E-mail')).toBeTruthy();
+    expect(getByPlaceholderText('Senha')).toBeTruthy();
+  });
+});
